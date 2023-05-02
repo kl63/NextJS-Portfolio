8985cf89e3a8045b6dfbe3f4b4eb21f8fbc9d37e
kl63 <kl63@NJIT.edu>
2023-05-02 19:02:40 -0400

    compressing 4

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,46 +1,44 @@
-const fetch = require('node-fetch');
-
 const subscribe = async (req, res) => {
+  const { email } = req.body
+
+  if (req.method !== 'POST') {
+    return res.status(405).send({ error: 'Request method is not allowed.' })
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required.' })
+  }
+
   try {
-    const { email } = req.body;
-
-    if (!email) {
-      return res.status(400).json({ error: 'Email is required.' });
-    }
-
-    if (req.method !== 'POST') {
-      return res.status(405).send({ error: 'Request method is not allowed.' });
-    }
-
-    const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY;
-    const MAILCHIMP_LIST_ID = process.env.MAILCHIMP_LIST_ID;
+    const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY
+    const MAILCHIMP_LIST_ID = process.env.MAILCHIMP_LIST_ID
     const MAILCHIMP_DC = process.env.MAILCHIMP_DC;
-    const API_URL = `https://${MAILCHIMP_DC}.api.mailchimp.com/3.0/lists/${MAILCHIMP_LIST_ID}/members`;
+
+    const API_URL = `https://${MAILCHIMP_DC}.api.mailchimp.com/3.0/lists/${MAILCHIMP_LIST_ID}/members`
 
     const data = {
       email_address: email,
       status: 'subscribed',
-    };
+    }
 
     const response = await fetch(API_URL, {
-      method: 'POST',
+      body: JSON.stringify(data),
       headers: {
         'Authorization': `apikey ${MAILCHIMP_API_KEY}`,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(data)
-    });
+      method: 'POST',
+    })
 
-    if (!response.ok) {
-      const error = await response.json();
-      throw new Error(error.detail || 'There was an error subscribing to the list.');
+    if (response.status >= 400) {
+      const error = await response.json()
+      return res.status(400).json({ error: error.detail || 'There was an error subscribing to the list.' })
     }
 
-    return res.status(201).json({ message: 'Subscribed successfully' });
+    return res.status(201).json({ error: '' })
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: error.message || error.toString() });
+    return res.status(500).json({ error: error.message || error.toString() })
   }
-};
+}
 
-export default subscribe;
+export default subscribe
