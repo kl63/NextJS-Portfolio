465afc23e90e8d21e536bd259ff2e918c36035b5
kl63 <kl63@NJIT.edu>
2023-05-02 20:20:08 -0400

    fixing MC 3

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,21 +1,20 @@
-import fetch from 'isomorphic-unfetch';
+import fetch from 'isomorphic-unfetch'
 
 const subscribe = async (req, res) => {
+  const { email } = req.body
+
   if (req.method !== 'POST') {
     return res.status(405).send({ error: 'Request method is not allowed.' })
   }
 
-  const { email } = req.body;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (!email || !emailRegex.test(email)) {
-    return res.status(400).json({ error: 'Please provide a valid email address.' })
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required.' })
   }
 
   try {
     const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY
     const MAILCHIMP_LIST_ID = process.env.MAILCHIMP_LIST_ID
-    const MAILCHIMP_DC = process.env.MAILCHIMP_DC;
+    const MAILCHIMP_DC = process.env.MAILCHIMP_DC
 
     const API_URL = `https://${MAILCHIMP_DC}.api.mailchimp.com/3.0/lists/${MAILCHIMP_LIST_ID}/members`
 
@@ -33,17 +32,15 @@ const subscribe = async (req, res) => {
       method: 'POST',
     })
 
-    if (response.status >= 400) {
+    if (response.status === 200 || response.status === 204) {
+      return res.status(200).json({ success: true })
+    } else {
       const error = await response.json()
-      console.error(error)
       return res.status(400).json({ error: error.detail || 'There was an error subscribing to the list.' })
     }
-
-    return res.status(201).json({ error: '' })
   } catch (error) {
-    console.error(error)
-    return res.status(500).json({ error: 'Something went wrong. Please try again later.' })
+    return res.status(500).json({ error: error.message || error.toString() })
   }
 }
 
-export default subscribe;
+export default subscribe
