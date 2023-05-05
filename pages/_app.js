5a7287ce368a64bf18bf27225597a050098a1392
kl63 <kl63@NJIT.edu>
2023-05-05 10:31:37 -0400

    GA testing 2

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,7 @@ import React, { useEffect } from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import MDXComponents from '@/components/MDX';
 import '@/styles/globals.css';
-import ReactGA from 'react-ga';
 import { useRouter } from 'next/router';
-
-// Access the tracking ID from the environment variable
-const trackingId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID;
 
 function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
@@ -14,10 +10,11 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    if (trackingId) {
-      ReactGA.initialize(trackingId);
+    if (process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID) {
       const handleRouteChange = (url) => {
-        ReactGA.pageview(url);
+        window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID, {
+          page_path: url,
+        });
       };
 
       // Track pageviews when route changes
@@ -27,7 +24,7 @@ function MyApp({ Component, pageProps }) {
         router.events.off('routeChangeComplete', handleRouteChange);
       };
     }
-  }, [router.events, trackingId]);
+  }, [router.events]);
 
   return (
     <MDXProvider components={MDXComponents}>{getLayout(<Component {...pageProps} />)}</MDXProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { config } from '../theme.config'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { config } from '../theme.config';
 
 class MyDocument extends Document {
   render() {
@@ -12,14 +12,32 @@ class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-5.png" />
           <link rel="manifest" href="/favicon/site.webmanifest" />
           <link rel="alternate" type="application/rss+xml" href="/feed/blog/feed.xml" />
+          
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID && (
+            <>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}`}></script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body className="scrollbar-thin scrollbar-thumb-omega-500 hover:scrollbar-thumb-omega-600">
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
