9f61c24d02fc0b8c9c85922860d211cd8d1778b7
kl63 <kl63@NJIT.edu>
2023-05-05 08:18:55 -0400

    added GA

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,18 @@ import { MDXProvider } from '@mdx-js/react'
 import MDXComponents from '@/components/MDX'
 import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import ReactGA from 'react-ga';
 
 function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
+  ReactGA.initialize('G-YZGEFEJ0PW');
+
   return (
-    
-    <MDXProvider components={MDXComponents}>{getLayout(<Component {...pageProps} />)}</MDXProvider>
+    <Analytics id="G-YZGEFEJ0PW">
+      <MDXProvider components={MDXComponents}>{getLayout(<Component {...pageProps} />)}</MDXProvider>
+    </Analytics>
   )
 }
 
